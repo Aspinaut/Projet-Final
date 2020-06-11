2f1d5ac9daab79e7d5082325fa97c5a15f342df4
Reivnaj <pelletierjanvier@gmail.com>
2020-06-11 22:39:02 +0200

    add gem simple_form and gem act_as_taggable_on to setup mode filter

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,5 +1,12 @@
 class TrainingsController < ApplicationController
+
   def index
-    @trainings = Training.all
+    if params[:search]
+      @filter = params[:search][:modes].flatten.reject(&:blank?)
+      @trainings = @filter.empty? ? Training.all : Training.all.tagged_with(@filter, any: true)
+    else
+      @trainings = Training.all
+    end
   end
+
 end
--- app/models/training.rb
+++ app/models/training.rb
@@ -3,4 +3,24 @@ class Training < ApplicationRecord
   has_many :comments
   has_many :sessions
   has_many :programs
+
+  acts_as_taggable_on :tags
+  $modes = ["remote", "onsite", "remote or onsite", "remote-alternance"]
+  $duration = []
+  # def filter_by_mode
+  #
+  # end
+  #
+  # def filter_by_location
+  #
+  # end
+  #
+  # def filter_by_language
+  #
+  # end
+  #
+  # def filter_by_price
+  #
+  # end
+
 end

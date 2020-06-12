815849e46831a533840a86f539d5f1f5cf5d0b9a
Vincent Masse <vincentmasse3@gmail.com>
2020-06-12 08:44:57 +0200

    add AJAX to filters

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -2,10 +2,30 @@ class TrainingsController < ApplicationController
 
   def index
     if params[:search]
-      @filter = params[:search][:modes].flatten.reject(&:blank?)
-      @trainings = @filter.empty? ? Training.all : Training.all.tagged_with(@filter, any: true)
+      @filter = params[:search][:modes].concat(params[:search][:durations]).flatten.reject(&:blank?)
+      # puts "$" * 60
+      # puts params[:search][:modes].count
+      # puts params[:search][:durations].count
+      # puts "$" * 60
+      if @filter.empty?
+        @trainings = Training.all
+      elsif @filter.count == 1
+        @trainings = Training.all.tagged_with(@filter, any: true)
+      elsif @filter.count > 1
+        # if params[:search][:modes].count > 1 && params[:search][:durations].count == 0
+        #   @trainings = Training.all.tagged_with(@filter, any: true)
+        # elsif params[:search][:durations].count > 1 && params[:search][:modes].count == 0
+        #   @trainings = Training.all.tagged_with(@filter, any: true)
+        # elsif params[:search][:durations].count >= 1 && params[:search][:modes].count >= 1
+        @trainings = Training.all.tagged_with(@filter, match_all: true)
+        # end
+      end
     else
       @trainings = Training.all
+    end
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 

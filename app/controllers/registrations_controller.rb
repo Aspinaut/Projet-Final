9f109c881decf94cc1b4d99cfff7fa4ca0b25ff9
Vincent Masse <vincentmasse3@gmail.com>
2020-06-13 18:32:39 +0200

    rectify bug with users names

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,13 +5,20 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     super
-    User.update(first_name:params[:user][:first_name], last_name:params[:user][:last_name])
     # add custom create logic here
     #User.create(email:params[:email], password:params[:password], password_confirmation:params[:password_confirmation], first_name:params[:first_name], last_name:params[:last_name])
   end
 
   def update
     super
-    User.update(first_name:params[:user][:first_name], last_name:params[:user][:last_name])
+    User.find(current_user.id). update(first_name:params[:user][:first_name], last_name:params[:user][:last_name])
   end
+  private
+
+    def sign_up_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :current_password)
+    end
+    def account_update_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :current_password)
+    end
 end

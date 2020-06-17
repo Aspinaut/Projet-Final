57be4a361c2e0d039eadc3f571becb0cce1f38fb
Vincent Masse <vincentmasse3@gmail.com>
2020-06-17 20:02:10 +0200

    try to set checkbox as radio button

--- app/models/training.rb
+++ app/models/training.rb
@@ -8,7 +8,7 @@ class Training < ApplicationRecord
 
   $modes = ["à distance", "présentiel", "à distance et en alternance", "à distance ou présentiel"]
   $durations = ["1-10", "11-20", "21-30", "31-40", "41+"]
-  $prices = ["0-1000", "1001-2000", "2001-3000", "3001-4000", "4000+"]
+  $prices = [ "0-1000", "1001-2000", "2001-3000", "3001-4000", "4000+"]
   # def filter_by_mode
   #
   # end

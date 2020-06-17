160599e35001b74db03f3647ccda8d884e2e30ea
Vincent Masse <vincentmasse3@gmail.com>
2020-06-17 17:55:18 +0200

    add 3rd filter

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -4,14 +4,13 @@ class TrainingsController < ApplicationController
 
     if params[:search]
       @filter = params[:search][:modes].concat(params[:search][:durations]).concat(params[:search][:prices]).flatten.reject(&:blank?)
-       puts "$" * 60
-       puts @filter
-       puts "$" * 60
       if @filter.empty?
         @trainings = Training.all
       elsif @filter.count == 1
         @trainings = Training.all.tagged_with(@filter, any: true)
-      elsif @filter.count > 1
+      elsif @filter.count == 2
+        @trainings = Training.all.tagged_with(@filter)
+      elsif @filter.count == 3
         @trainings = Training.all.tagged_with(@filter, match_all: true)
       end
     else

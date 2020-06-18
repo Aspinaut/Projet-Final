83fd18c3b06fca641f56a5c636cf3f28fd4082ce
Vincent Masse <vincentmasse3@gmail.com>
2020-06-18 08:54:12 +0200

    add basic front to filters

--- app/models/training.rb
+++ app/models/training.rb
@@ -9,20 +9,5 @@ class Training < ApplicationRecord
   $modes = ["à distance", "présentiel", "à distance et en alternance", "à distance ou présentiel"]
   $durations = ["1-10", "11-20", "21-30", "31-40", "41+"]
   $prices = [ "0-1000", "1001-2000", "2001-3000", "3001-4000", "4000+"]
-  # def filter_by_mode
-  #
-  # end
-  #
-  # def filter_by_location
-  #
-  # end
-  #
-  # def filter_by_language
-  #
-  # end
-  #
-  # def filter_by_price
-  #
-  # end
 
 end
